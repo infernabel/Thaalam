410f35f91d2ef213e204d2bcae0c5ff4030f3e5e
infernabel <161008355+infernabel@users.noreply.github.com>
2025-03-15 17:06:51 +0530

    Update config.js

--- config.js
+++ config.js
@@ -25,10 +25,10 @@ module.exports = {
   errorLog: "", 
   nodes: [
      {
-      name: "GlaceYT",
-      password: "glaceyt",
-      host: "193.226.78.187",
-      port:  9372,
+      name: "Avinan",
+      password: "pokemon",
+      host: "new-york-node-1.vortexcloud.xyz",
+      port:  9376,
       secure: false
     }
   ]
