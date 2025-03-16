d219802816393281a25f750b0e88fcd70a419909
infernabel <161008355+infernabel@users.noreply.github.com>
2025-03-16 13:08:13 +0530

    Update musicicons.js

--- UI/icons/musicicons.js
+++ UI/icons/musicicons.js
@@ -1,20 +1,28 @@
 const musicIcons = {
-    footerIcon: 'https://cdn.discordapp.com/emojis/865916418909536276.gif', 
-    correctIcon: 'https://cdn.discordapp.com/emojis/820971058089820200.gif', 
-    playerIcon: 'https://cdn.discordapp.com/emojis/992051186188435467.gif',
-    pauseresumeIcon:'https://cdn.discordapp.com/emojis/982935235807313971.gif',
-    loopIcon: 'https://cdn.discordapp.com/emojis/749272851529334795.gif',
-    beatsIcon: 'https://cdn.discordapp.com/emojis/928310693416009828.gif',
-    beats2Icon : 'https://cdn.discordapp.com/emojis/953413565400879165.gif',
-    alertIcon : 'https://cdn.discordapp.com/emojis/996431685358981201.gif',
-    skipIcon: 'https://cdn.discordapp.com/emojis/938388856095514654.gif',
-    stopIcon: 'https://cdn.discordapp.com/emojis/1021628438441902100.gif',
-    volumeIcon: 'https://cdn.discordapp.com/emojis/1040824501711159397.gif',
-    playlistIcon : 'https://cdn.discordapp.com/emojis/1096444591982522498.gif',
-    heartIcon : 'https://cdn.discordapp.com/emojis/900257798003240961.gif',
-    pingIcon : 'https://cdn.discordapp.com/emojis/923089856752664576.gif',
-    CheckmarkIcon: "https://cdn.discordapp.com/emojis/819446784647757834.gif",
-    MusicIcon:"https://cdn.discordapp.com/emojis/763415718271385610.gif",
+    footer: 'https://cdn.discordapp.com/emojis/865916418909536276.gif', 
+    correct: 'https://cdn.discordapp.com/emojis/820971058089820200.gif', 
+
+    // Player Controls
+    player: 'https://cdn.discordapp.com/emojis/992051186188435467.gif',
+    pauseResume: 'https://cdn.discordapp.com/emojis/982935235807313971.gif',
+    loop: 'https://cdn.discordapp.com/emojis/749272851529334795.gif',
+    skip: 'https://cdn.discordapp.com/emojis/938388856095514654.gif',
+    stop: 'https://cdn.discordapp.com/emojis/1021628438441902100.gif',
+    volume: 'https://cdn.discordapp.com/emojis/1040824501711159397.gif',
+
+    // Beats & Effects
+    beats1: 'https://cdn.discordapp.com/emojis/928310693416009828.gif',
+    beats2: 'https://cdn.discordapp.com/emojis/953413565400879165.gif',
+
+    // Alerts & Status
+    alert: 'https://cdn.discordapp.com/emojis/996431685358981201.gif',
+    checkmark: "https://cdn.discordapp.com/emojis/819446784647757834.gif",
+
+    // Other Icons
+    playlist: 'https://cdn.discordapp.com/emojis/1096444591982522498.gif',
+    heart: 'https://cdn.discordapp.com/emojis/900257798003240961.gif',
+    ping: 'https://cdn.discordapp.com/emojis/923089856752664576.gif',
+    music: "https://cdn.discordapp.com/emojis/763415718271385610.gif",
 };
 
 module.exports = musicIcons;
